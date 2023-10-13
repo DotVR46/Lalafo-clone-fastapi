81045ac1bc9da8db383cb2370b8054a23d1aa4d8
DotVR46 <maggot8950@gmail.com>
2023-10-13 20:22:01 +0600

    Настройка alembic и подключения к DB

---
+++ core/database/models/__init__.py
@@ -0,0 +1,17 @@
+__all__ = (
+    "Base",
+    "DatabaseHelper",
+    "db_helper",
+    # "Item",
+    # "User",
+    # "Post",
+    # "Profile",
+)
+
+from .base import Base
+from core.database.db_helper import DatabaseHelper, db_helper
+
+# from .item import Item
+# from .user import User
+# from .post import Post
+# from .profile import Profile
