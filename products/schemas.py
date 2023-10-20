1b47efb0ce7557769af35266e33c7231d25d5aab
DotVR46 <maggot8950@gmail.com>
2023-10-21 00:44:08 +0600

    Схема Category

--- products/schemas.py
+++ products/schemas.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
 
 
+class CategoryBase(BaseModel):
+    id: int
+    title: str
+    description: str
+
+
 class ProductBase(BaseModel):
     title: str
     description: str
     city: str
     price: int
     currency: str
-    category_id: int
+    category: CategoryBase
