f1e54b0f5285601dc2942ed04dc4b301a6135108
DotVR46 <maggot8950@gmail.com>
2023-10-26 01:22:35 +0600

    crud и view для получения одного продукта

--- products/crud.py
+++ products/crud.py
@@ -1,14 +1,26 @@
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import joinedload, selectinload
 
-from core.database.models import Product
+from core.database.models import Product, Category
 from .schemas import ProductBase
 
 
 async def get_products(session: AsyncSession) -> list[Product]:
-    stmt = select(Product).options(joinedload(Product.category), joinedload(Product.user)).order_by(Product.id)
+    stmt = (
+        select(Product)
+        .options(joinedload(Product.category), joinedload(Product.user))
+        .order_by(Product.id)
+    )
     result: Result = await session.execute(stmt)
     products = result.scalars().all()
     return list(products)
+
+
+async def get_single_product(session: AsyncSession, product_id: int) -> Product | None:
+    return await session.get(
+        Product,
+        product_id,
+        options=(joinedload(Product.category), joinedload(Product.user)),
+    )
--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.database.db_helper import db_helper
 
 from . import crud
+from .dependencies import get_product_by_id
 from .schemas import Product
 
 router = APIRouter()
@@ -14,3 +15,10 @@ async def get_products(
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.get_products(session=session)
+
+
+@router.get("/{product_id}", response_model=Product)
+async def get_single_product(
+    product: Product = Depends(get_product_by_id),
+):
+    return product
